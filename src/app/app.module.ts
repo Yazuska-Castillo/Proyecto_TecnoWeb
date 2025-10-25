01ab2db24dfdb5d331ea29e7f8d48eaf4813fc17
Yazuska Castillo <yazuska.castillo.cuevas@alumnos.uta.cl>
2025-10-25 19:07:41 -0300

    Se añade componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
