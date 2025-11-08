97f8b9aa2f9ab4f6800a18a2b29630d5842ef9f1
ReneAycaV <rene.ayca.vargas@alumnos.uta.cl>
2025-11-08 12:30:23 -0300

    Correcciones en main: imports, FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';  //investigar
+import { FormsModule } from '@angular/forms'; // ya correcto
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,7 @@ import { CatalogoHabitacionesComponent } from './components/habitaciones/catalog
 import { DetalleHabitacionComponent } from './components/habitaciones/detalle-habitacion/detalle-habitacion.component';
 import { PanelClienteComponent } from './components/cliente/panel-cliente/panel-cliente.component';
 import { HistorialReservasComponent } from './components/cliente/historial-reservas/historial-reservas.component';
+import { GestionHotelesComponent } from './components/Admin/gestion-hoteles/gestion-hoteles.component'; // 👈 agrega esta línea
 
 @NgModule({
   declarations: [
@@ -16,12 +17,12 @@ import { HistorialReservasComponent } from './components/cliente/historial-reser
     DetalleHabitacionComponent,
     PanelClienteComponent,
     HistorialReservasComponent,
-   
+    GestionHotelesComponent // xd
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule  //investigar
+    FormsModule //  [(ngModel)]
   ],
   providers: [],
   bootstrap: [AppComponent],
