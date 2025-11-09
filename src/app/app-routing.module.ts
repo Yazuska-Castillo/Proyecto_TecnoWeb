194a0d49ba46d909429e597a91bdb701c8b6e2ab
Yazuska Castillo <yazuska.castillo.cuevas@alumnos.uta.cl>
2025-11-09 15:12:22 -0300

    Se Implementa la tabla de hotel, botones para Agregar, Eliminar y Editar para la gestión de hoteles y se modifican las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// Componentes
+import { LoginComponent } from './components/login/login.component';
 import { GestionPromocionesComponent } from './components/gestion-promociones/gestion-promociones.component';
 import { GestionHotelesComponent } from './components/Admin/gestion-hoteles/gestion-hoteles.component';
-//import { LoginComponent } from './components/login/login.component';  (coloquen bien la ruta si lo van a usar)
 import { CatalogoHabitacionesComponent } from './components/habitaciones/catalogo-habitaciones/catalogo-habitaciones.component';
 import { DetalleHabitacionComponent } from './components/habitaciones/detalle-habitacion/detalle-habitacion.component';
 import { PanelClienteComponent } from './components/cliente/panel-cliente/panel-cliente.component';
 import { HistorialReservasComponent } from './components/cliente/historial-reservas/historial-reservas.component';
 
 const routes: Routes = [
-  { path: '', component: CatalogoHabitacionesComponent },
+  { path: '', component: LoginComponent, pathMatch: 'full' }, //Ahora login es el principal, falta la autenticación
+
+  // ---------------- CLIENTE ----------------
+  { path: 'catalogo', component: CatalogoHabitacionesComponent },
   { path: 'habitacion/:id', component: DetalleHabitacionComponent },
+
   {
     path: 'cliente',
     component: PanelClienteComponent,
     children: [
-      { path: 'historial', component: HistorialReservasComponent }
-    ]
+      { path: 'historial', component: HistorialReservasComponent },
+      { path: '', redirectTo: 'historial', pathMatch: 'full' },
+    ],
   },
-  { path: '', redirectTo: 'gestion-promociones', pathMatch: 'full' },
+  { path: 'cliente/datos', component: GestionPromocionesComponent },
+
+  // ---------------- ADMIN ----------------
+  { path: 'admin', component: GestionHotelesComponent },
   { path: 'gestion-promociones', component: GestionPromocionesComponent },
-  { path: '**', redirectTo: 'gestion-promociones' }
+
+  // ---------------- ERRORES ----------------
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
@@ -28,5 +40,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
-
-
